8906d0e5bbb1cbd8100c61a785944551ca0bda1a
Victoria Cheng <35031228+victoriacheng15@users.noreply.github.com>
2023-03-31 12:45:48 -0600

    fix: correct tests folder name and move to src folder (#5)

--- src/__tests__/home.test.tsx
+++ src/__tests__/home.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import Home from "../page";
+import Home from "../app/page";
 
 describe("home", () => {
 	it("should render the heading", () => {
