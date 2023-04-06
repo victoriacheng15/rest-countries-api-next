2d7bd6c0f300b715352fb2e780214ba22a897ec9
Victoria Cheng <victoria.cheng15@gmail.com>
2023-04-06 10:07:30 -0600

    chore: add useGetAllCountriesQuery to check fetched data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
+"use client";
+import { useGetAllCountriesQuery } from "../redux/services/apiSlice";
+
 export default function Home() {
-	return (
-		<main>
-			<h1 className='text-2xl text-blue-800 underline'>
-				tailwind css with nextjs
-			</h1>
-		</main>
-	);
+  const { data, error, isLoading } = useGetAllCountriesQuery();
+  console.log({ data, error, isLoading });
+
+  return (
+    <main>
+      <h1 className="text-2xl text-blue-800 underline">
+        tailwind css with nextjs
+      </h1>
+    </main>
+  );
 }
