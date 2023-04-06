e2401c927b52d0fdbccf52a91eed8b17d944f86c
Victoria Cheng <victoria.cheng15@gmail.com>
2023-04-06 14:24:29 -0600

    chore: populate display with data for mocked test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,33 @@
 import { useGetAllCountriesQuery } from "../redux/services/apiSlice";
 
 export default function Home() {
-	const { data, error, isLoading } = useGetAllCountriesQuery();
-	console.log({ data, error, isLoading });
+	const { data: countries, isLoading, error } = useGetAllCountriesQuery();
+
+	if (isLoading) {
+		return <p>Loading...</p>;
+	}
+
+	if(error) {
+		return <p>somwthing went wrong</p>
+	}
 
 	return (
 		<main>
 			<h1 className="text-2xl text-blue-800 underline">
 				tailwind css with nextjs
 			</h1>
+			<section>
+				{
+					countries?.slice(0,3).map((country) => (
+						<div key={country.name.common}>
+							<h2>{country.name.common}</h2>
+							<p>capital: {country.capital}</p>
+							<p>population: {country.population}</p>
+							<img src={country.flags.png} alt={country.name.common} />
+						</div>
+					))
+				}
+			</section>
 		</main>
 	);
 }
