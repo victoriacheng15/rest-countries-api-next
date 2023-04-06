0852ce81bbb7f34c7e8833b6de1ebfd3b4058f1b
Victoria Cheng <victoria.cheng15@gmail.com>
2023-04-06 14:24:57 -0600

    feat: add custom render for redux store and add tests to check msw

--- src/__tests__/home.test.tsx
+++ src/__tests__/home.test.tsx
@@ -1,13 +1,34 @@
-import { render, screen } from "@testing-library/react";
+import { screen, waitFor } from "@testing-library/react";
+import { renderWithProviders as render } from "@/test-utils";
+import { server } from "../mocks/server";
+import { rest } from "msw";
 import Home from "../app/page";
 
 describe("home", () => {
-	it("should render the heading", () => {
-		render(<Home />);
+  it("render loading status", async () => {
+    render(<Home />);
+    const loadingText = await screen.findByText(/Loading/i);
+    expect(loadingText).toBeInTheDocument();
+  });
 
-		const heading = screen.getByRole("heading", {
-			name: /tailwind css with nextjs/i,
-		});
-		expect(heading).toBeInTheDocument();
-	});
+  it("remder componet without countries", async () => {
+    server.use(
+      rest.get("https://restcountries.com/v3.1/all", (req, res, ctx) => {
+        return res(ctx.status(500), ctx.json([]));
+      }),
+    );
+    render(<Home />);
+    const heading = await screen.findByRole("heading", {
+      level: 1,
+    });
+    expect(heading).toBeInTheDocument();
+  });
+
+  it("render countries", async () => {
+    render(<Home />);
+    const heading = await screen.findAllByRole("heading", { level: 2 });
+    await waitFor(async () => {
+      expect(heading).toHaveLength(3);
+    });
+  });
 });
