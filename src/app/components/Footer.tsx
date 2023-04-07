e9875892a17ad00285d186402a14ca4905a01fdc
victoriacheng15 <victoriacheng15@users.noreply.github.com>
2023-04-07 22:40:31 +0000

    style: the code has been formatted!

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -5,9 +5,21 @@ function Footer() {
 	return (
 		<footer className="py-4 bg-slate-100 dark:bg-gray-800 dark:text-slate-100">
 			<p className="flex items-center justify-center gap-2">
-				Coded by Victoria <SocialIcon href="https://github.com/victoriacheng15/rest-countries-next#readme" icon={<SiGithub />} /> |{" "}
-				<SocialIcon href="https://www.linkedin.com/in/victoriacheng15/" icon={<SiLinkedin />} /> |{" "}
-				<SocialIcon href="https://twitter.com/viktoriacheng15" icon={<SiTwitter />} />{" "}
+				Coded by Victoria{" "}
+				<SocialIcon
+					href="https://github.com/victoriacheng15/rest-countries-next#readme"
+					icon={<SiGithub />}
+				/>{" "}
+				|{" "}
+				<SocialIcon
+					href="https://www.linkedin.com/in/victoriacheng15/"
+					icon={<SiLinkedin />}
+				/>{" "}
+				|{" "}
+				<SocialIcon
+					href="https://twitter.com/viktoriacheng15"
+					icon={<SiTwitter />}
+				/>{" "}
 			</p>
 		</footer>
 	);
