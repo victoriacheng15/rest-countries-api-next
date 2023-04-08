1f4a6072b488ce7d0330501d660f44b431faab32
Victoria Cheng <victoria.cheng15@gmail.com>
2023-04-08 16:09:38 -0600

    chore: update tests for home page

--- src/__tests__/home.test.tsx
+++ src/__tests__/home.test.tsx
@@ -1,34 +1,12 @@
-import { screen, waitFor } from "@testing-library/react";
+import { screen } from "@testing-library/react";
 import { renderWithProviders as render } from "@/test-utils";
-import { server } from "../mocks/server";
-import { rest } from "msw";
 import Home from "../app/page";
 
 describe("home", () => {
-	it("render loading status", async () => {
+	it("should render card components", async () => {
 		render(<Home />);
-		const loadingText = await screen.findByText(/Loading/i);
-		expect(loadingText).toBeInTheDocument();
-	});
 
-	it("remder componet without countries", async () => {
-		server.use(
-			rest.get("https://restcountries.com/v3.1/all", (req, res, ctx) => {
-				return res(ctx.status(500), ctx.json([]));
-			}),
-		);
-		render(<Home />);
-		const heading = await screen.findByRole("heading", {
-			level: 1,
-		});
-		expect(heading).toBeInTheDocument();
-	});
-
-	it("render countries", async () => {
-		render(<Home />);
-		const heading = await screen.findAllByRole("heading", { level: 2 });
-		await waitFor(async () => {
-			expect(heading).toHaveLength(3);
-		});
+		const cards = await screen.findAllByRole("article");
+		expect(cards).toHaveLength(12);
 	});
 });
