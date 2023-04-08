16a638e5331ef8a71ad37502ef6b13f19f9ceb88
Victoria Cheng <victoria.cheng15@gmail.com>
2023-04-08 15:48:38 -0600

    feat: add card skeleton for loading status

---
+++ src/app/components/CardSkeleton.tsx
@@ -0,0 +1,17 @@
+function CardSkeleton() {
+	return (
+		<article data-testid="skeleton"
+			className="h-[356px] overflow-hidden border-2 border-gray-700 dark:border-slate-100 rounded-lg"
+		  >
+			<div className="w-full h-[180px] bg-gray-300" />
+			<div className="p-3 space-y-4">
+				<div className="w-full h-8 bg-gray-300" />
+				<div className="h-6 bg-gray-300" />
+				<div className="h-6 bg-gray-300" />
+				<div className="h-6 bg-gray-300" />
+			</div>
+		</article>
+	);
+}
+
+export default CardSkeleton;
