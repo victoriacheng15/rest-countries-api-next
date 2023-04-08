c38c2a1938d3ac5f266d6600204eb39536213f62
Victoria Cheng <victoria.cheng15@gmail.com>
2023-04-08 17:27:17 -0600

    feat: standardize button style for hover, light and dakr mode

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -19,7 +19,7 @@ function Header({ mode, toggleDark }: HeaderProps) {
 							type="button"
 							onClick={toggleDark}
 							title={buttonTitle}
-							className="p-2 text-2xl capitalize border-2 border-gray-900 rounded-xl dark:border-slate-100"
+							className="p-2 text-2xl capitalize duration-300 ease-in-out border-2 border-gray-900 rounded-xl dark:border-slate-100 dark:text-slate-100 hover:bg-gray-900 hover:text-slate-100 dark:hover:bg-slate-100 dark:hover:text-gray-900"
 						>
 							{buttonText}
 						</button>
