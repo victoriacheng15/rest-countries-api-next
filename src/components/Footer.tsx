400f2eb1c9923bb8e7aa3af8fe436b32d03cc7cb
Victoria Cheng <victoria.cheng15@gmail.com>
2023-04-10 17:28:58 -0600

    fix: add aria label for social links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,16 +7,19 @@ function Footer() {
 			<p className="flex items-center justify-center gap-2">
 				Coded by Victoria{" "}
 				<SocialIcon
+					label="github"
 					href="https://github.com/victoriacheng15/rest-countries-next#readme"
 					icon={<SiGithub />}
 				/>{" "}
 				|{" "}
 				<SocialIcon
+					label="Linkedin"
 					href="https://www.linkedin.com/in/victoriacheng15/"
 					icon={<SiLinkedin />}
 				/>{" "}
 				|{" "}
 				<SocialIcon
+					label="Twitter"
 					href="https://twitter.com/viktoriacheng15"
 					icon={<SiTwitter />}
 				/>{" "}
@@ -27,15 +30,9 @@ function Footer() {
 
 export default Footer;
 
-function SocialIcon({
-	href,
-	icon,
-}: {
-	href: string;
-	icon: React.ReactNode;
-}) {
+function SocialIcon({ href, icon, label }: SocialIconProps) {
 	return (
-		<a href={href} target="_blank" rel="noreferrer">
+		<a aria-label={label} href={href} target="_blank" rel="noreferrer">
 			{icon}
 		</a>
 	);
