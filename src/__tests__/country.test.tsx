d5b184bd2ed888a3fa1e0f39c570bfe412624b13
victoriacheng15 <victoriacheng15@users.noreply.github.com>
2023-04-10 04:10:07 +0000

    style: the code has been formatted!

--- src/__tests__/country.test.tsx
+++ src/__tests__/country.test.tsx
@@ -38,52 +38,49 @@ const country = [
 describe("CountryPage", () => {
 	it("should render title as country official name", () => {
 		render(<Country {...country[0]} />);
-		const title = screen.getByRole("heading", {level: 2});
+		const title = screen.getByRole("heading", { level: 2 });
 		expect(title).toBeInTheDocument();
 	});
 
-  it("should render population", () => {
+	it("should render population", () => {
 		render(<Country {...country[0]} />);
 		const population = screen.getByText(/population/i);
 		expect(population).toBeInTheDocument();
 	});
 
-  it("should render region", () => {
+	it("should render region", () => {
 		render(<Country {...country[0]} />);
 		const regions = screen.getAllByText(/region/i);
 		expect(regions).toHaveLength(2);
 	});
 
-
-  it("should render capital", () => {
+	it("should render capital", () => {
 		render(<Country {...country[0]} />);
 		const capital = screen.getByText(/capital/i);
 		expect(capital).toBeInTheDocument();
 	});
 
-  it("should render top level domain", () => {
+	it("should render top level domain", () => {
 		render(<Country {...country[0]} />);
 		const tld = screen.getByText(/top level domain/i);
 		expect(tld).toBeInTheDocument();
 	});
 
-  it("should render currencies", () => {
+	it("should render currencies", () => {
 		render(<Country {...country[0]} />);
 		const currencies = screen.getByText(/currencies/i);
 		expect(currencies).toBeInTheDocument();
 	});
 
-  it("should render languages", () => {
+	it("should render languages", () => {
 		render(<Country {...country[0]} />);
 		const languages = screen.getByText(/languages/i);
 		expect(languages).toBeInTheDocument();
 	});
 
-  it("should render borders title", () => {
+	it("should render borders title", () => {
 		render(<Country {...country[0]} />);
-		const bordersTitle = screen.getByRole("heading", {level: 3});
+		const bordersTitle = screen.getByRole("heading", { level: 3 });
 		expect(bordersTitle).toBeInTheDocument();
 	});
-
-  
 });
