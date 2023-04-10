39a732601efe5cd7d6c1aae411c5a04671970e04
Victoria Cheng <victoria.cheng15@gmail.com>
2023-04-10 15:12:14 -0600

    refactor: extract all props type declarations to props.d.ts

--- src/types/props.d.ts
+++ src/types/props.d.ts
@@ -1,12 +1,36 @@
-interface HeaderProps {
-	mode: boolean;
-	toggleDark: () => void;
+interface BaseProps {
+	onClick: () => void;
 }
 
-interface FromProps {
-	onClick: () => void;
+interface BaseClasses {
+	classes: string;
+}
+
+interface ChildrenProps {
+	children: React.ReactNode;
+}
+
+interface HeaderProps extends BaseProps {
+	mode: boolean;
+}
+
+interface FromProps extends BaseProps {
 	search: string;
-	searchOnChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 	region: string;
+	searchOnChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 	selectOnChange: (e: ChangeEventHandler<HTMLSelectElement>) => void;
+}
+
+interface TitleProps extends BaseClasses {
+	title: string;
+}
+
+interface TextProps {
+	contentTitle: string;
+	content: string;
+}
+
+interface ButtonProps extends BaseProps, BaseClasses {
+	title?: string;
+	btnText: React.ReactNode;
 }
