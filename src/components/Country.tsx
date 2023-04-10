9aa645b2d8eb63c30244136ff6188576b6e200ec
Victoria Cheng <victoria.cheng15@gmail.com>
2023-04-10 13:54:04 -0600

    refactor: replace elements with Text, Title, Button, Containers components

--- src/components/Country.tsx
+++ src/components/Country.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import { useGetAllCountriesQuery } from "@/redux/services/apiSlice";
+import Title from "./Title";
+import Text from "./Text";
 
 function Country({
 	flags: { svg, alt },
@@ -9,8 +11,8 @@ function Country({
 	subregion,
 	capital,
 	tld,
-	currencies: money,
-	languages: lang,
+	currencies,
+	languages,
 	borders,
 }: Country) {
 	const { data: countries } = useGetAllCountriesQuery();
@@ -21,6 +23,18 @@ function Country({
 			.map(({ name }) => name.common);
 	}
 
+	const displayCurrencies = currencies
+		? Object.values(currencies)
+				.map(({ name }) => name)
+				.join(", ")
+		: "N/A";
+
+	const displayLangs = languages
+		? Object.values(languages)
+				.map((l) => l)
+				.join(", ")
+		: "N/A";
+
 	return (
 		<>
 			<Image
@@ -28,58 +42,27 @@ function Country({
 				alt={alt ?? official}
 				width={1600}
 				height={800}
-				className="w-1/2 max-w-xl"
+				className="w-full max-w-2xl md:w-1/2"
 			/>
-			<div className="w-11/12 space-y-4">
-				<h2 className="text-2xl font-bold">{official}</h2>
-				<p>
-					<span>population:</span>
-					{population}
-				</p>
-				<p>
-					<span>region:</span>
-					{region}
-				</p>
-				<p>
-					<span>sub region:</span>
-					{subregion}
-				</p>
-				<p>
-					<span>capital:</span>
-					{capital}
-				</p>
-				<p>
-					<span>top level domain:</span>
-					{tld.join(" | ")}
-				</p>
-				<p>
-					<span>currencies:</span>
-					{money
-						? Object.values(money)
-								.map(({ name }) => name)
-								.join(", ")
-						: "N/A"}
-				</p>
-				<p>
-					<span>languages:</span>
-					{lang
-						? Object.values(lang)
-								.map((l) => l)
-								.join(", ")
-						: "N/A"}
-				</p>
+			<div className="space-y-4">
+				<Title title={official} classes="text-4xl" />
+				<Text contentTitle="population" content={population.toLocaleString()} />
+				<Text contentTitle="region" content={region} />
+				<Text contentTitle="sub region" content={subregion} />
+				<Text contentTitle="capital" content={capital} />
+				<Text contentTitle="top level domain" content={tld.join(" | ")} />
+				<Text contentTitle="currencies" content={displayCurrencies} />
+				<Text contentTitle="languages" content={displayLangs} />
 				<h3 className="text-xl font-semibold">Borders:</h3>
 				<ul className="flex flex-wrap gap-4">
 					{borders
 						? borders.flatMap((border) => (
-								<>
-									<li
-										key={border}
-										className="p-2 bg-gray-900 text-slate-100 dark:bg-slate-100 dark:text-gray-900"
-									>
-										{getBorderName(border)}
-									</li>
-								</>
+								<li
+									key={border}
+									className="p-2 bg-gray-900 text-slate-100 dark:bg-slate-100 dark:text-gray-900"
+								>
+									{getBorderName(border)}
+								</li>
 						  ))
 						: "No border countries"}
 				</ul>
--- src/components/CountryCard.tsx
+++ src/components/CountryCard.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
+import Title from "./Title";
+import Text from "./Text";
 
 function CountryCard({
 	cca3,
@@ -14,7 +16,7 @@ function CountryCard({
 			href={`${cca3.toLowerCase()}?name=${official}`}
 			className="w-full h-full duration-300 ease-in-out sm:max-w-xs hover:scale-110"
 		>
-			<article className="h-full overflow-hidden border-2 border-gray-700 rounded-lg shadow-lg shadow-gray-900 flex-s dark:border-slate-100 dark:text-slate-300 dark:shadow-slate-500">
+			<article className="h-full overflow-hidden text-gray-900 border-2 border-gray-700 rounded-lg shadow-lg shadow-gray-900 dark:border-slate-100 dark:text-slate-300 dark:shadow-slate-500">
 				<Image
 					src={svg}
 					alt={alt ?? official}
@@ -23,19 +25,10 @@ function CountryCard({
 					className="object-cover w-full h-[180px]"
 				/>
 				<div className="p-3 space-y-4">
-					<h2 className="text-xl italic font-bold underline">{common}</h2>
-					<p>
-						<span className="text-base font-semibold">Population: </span>
-						{population.toLocaleString()}
-					</p>
-					<p>
-						<span className="text-base font-semibold">Region: </span>
-						{region}
-					</p>
-					<p>
-						<span className="text-base font-semibold">Capital: </span>
-						{capital ?? "N/A"}
-					</p>
+					<Title title={common} classes="text-xl" />
+					<Text contentTitle="population" content={population.toLocaleString()} />
+					<Text contentTitle="region" content={region} />
+					<Text contentTitle="capital" content={capital ?? "N/A"} />
 				</div>
 			</article>
 		</Link>
