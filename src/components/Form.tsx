b1371fd538172306a2f5d3241b24978371dee0d2
Victoria Cheng <victoria.cheng15@gmail.com>
2023-04-11 12:04:43 -0600

    chore: move regionList out to contants folder

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,4 +1,5 @@
 import Button from "./Button";
+import { regions } from "@/contants/regionList";
 
 function Form({
 	search,
@@ -7,29 +8,20 @@ function Form({
 	searchOnChange,
 	selectOnChange,
 }: FromProps) {
-	const regions = [
-		{ label: "Select Region", value: "" },
-		{ label: "Africa", value: "Africa" },
-		{ label: "Americas", value: "Americas" },
-		{ label: "Asia", value: "Asia" },
-		{ label: "Europe", value: "Europe" },
-		{ label: "Oceania", value: "Oceania" },
-	];
-
 	return (
-		<form className="flex flex-col justify-between gap-8 pb-10 md:flex-row">
+		<form className="flex flex-col gap-8 pb-10 md:flex-row">
 			<input
-				type="text"
-				name="search"
 				id="search"
+				name="search"
+				type="text"
 				placeholder='search for a country...'
 				value={search}
 				onChange={searchOnChange}
 				className="w-full p-3 placeholder-gray-600 bg-gray-200 rounded-xl md:w-96 outline-dashed outline-1 focus:outline-2 focus:outline-gray-500 dark:focus:outline-slate-100"
 			/>
 			<select
-				name="search"
 				id="select"
+				name="search"
 				value={region}
 				onChange={selectOnChange}
 				className="p-3 bg-gray-200 w-36 md:ml-auto rounded-xl outline-dashed outline-1 focus:outline-2 focus:outline-gray-500 dark:focus:outline-slate-100"
---
+++ src/contants/regionList.ts
@@ -0,0 +1,8 @@
+export const regions = [
+	{ label: "Select Region", value: "" },
+	{ label: "Africa", value: "Africa" },
+	{ label: "Americas", value: "Americas" },
+	{ label: "Asia", value: "Asia" },
+	{ label: "Europe", value: "Europe" },
+	{ label: "Oceania", value: "Oceania" },
+];
