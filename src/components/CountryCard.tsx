a769f692dd61288fc8652d6fa384960c44d40f38
Victoria Cheng <victoria.cheng15@gmail.com>
2023-04-20 17:21:58 -0600

    feat: replace Image with ImageContainer and remove Image import

--- src/components/CountryCard.tsx
+++ src/components/CountryCard.tsx
@@ -1,7 +1,7 @@
-import Image from "next/image";
 import Link from "next/link";
 import Title from "./Title";
 import Text from "./Text";
+import ImageContainer from "./ImageContainer";
 
 function CountryCard({
 	cca3,
@@ -19,11 +19,9 @@ function CountryCard({
 			className="w-full h-full duration-300 ease-in-out sm:max-w-[300px] hover:scale-105"
 		>
 			<article className="h-full overflow-hidden text-gray-900 border-2 border-gray-700 rounded-lg shadow-lg shadow-gray-900 dark:border-slate-100 dark:text-slate-100 dark:shadow-slate-500">
-				<Image
+				<ImageContainer
 					src={svg}
 					alt={alt ?? official}
-					width={1600}
-					height={800}
 					className="object-cover w-full h-[180px]"
 				/>
 				<div className="p-4 space-y-4">
